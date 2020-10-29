deda0a9724bbe94fa3be030faba4c894f149ac56
RikBala <rik280491@googlemail.com>
2020-10-29 17:53:47 +0000

    user input

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		// every View uses flexbox by default
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextInput placeholder="Course Goal" style={styles.input} />
+				<Button title="ADD" />
+			</View>
+			<View></View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	screen: {
+		padding: 50,
+	},
+	inputContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	input: {
+		width: "80%",
+		borderBottomColor: "black",
+		borderBottomWidth: 1,
+		padding: 10,
+	},
 });
