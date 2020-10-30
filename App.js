72c9b1b7b361588efb54cf7c096a44c5921fef9a
RikBala <rik280491@googlemail.com>
2020-10-30 16:31:34 +0000

    course goals CRUD

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
-import React from "react";
-import { StyleSheet, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	View,
+	Text,
+	TextInput,
+	Button,
+	ScrollView,
+	FlatList,
+} from "react-native";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const addGoalHandler = (goalTitle) => {
+		setCourseGoals((currentGoals) => [
+			...currentGoals,
+			{ id: Math.random().toString(), value: goalTitle },
+		]);
+	};
+
+	const removeGoalHandler = (goalId) => {
+		setCourseGoals((currentGoals) => {
+			return currentGoals.filter((goal) => goal.id !== goalId);
+		});
+	};
+
 	return (
 		// every View uses flexbox by default
 		<View style={styles.screen}>
-			<View style={styles.inputContainer}>
-				<TextInput placeholder="Course Goal" style={styles.input} />
-				<Button title="ADD" />
-			</View>
-			<View></View>
+			<GoalInput onAddGoal={addGoalHandler} />
+			{/* unlike web, RN not scrollable by default. Use ScrollView for this behaviour */}
+			{/* However, with ScrollView all items are loaded incl those not on the screen. FlatList is better for performance */}
+			<FlatList
+				keyExtractor={(item, index) => item.id}
+				data={courseGoals}
+				renderItem={(itemData) => (
+					<GoalItem
+						id={itemData.item.id}
+						onDelete={removeGoalHandler}
+						goalTitle={itemData.item.value}
+					/>
+				)}
+			/>
 		</View>
 	);
 }
@@ -17,16 +51,5 @@ export default function App() {
 const styles = StyleSheet.create({
 	screen: {
 		padding: 50,
-	},
-	inputContainer: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-	},
-	input: {
-		width: "80%",
-		borderBottomColor: "black",
-		borderBottomWidth: 1,
-		padding: 10,
 	},
 });
